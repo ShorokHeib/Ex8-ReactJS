c9d03f1d96eac3ed65685b3811af74a340d88966
unknown <shorok.heeb.99@gmail.com>
2024-03-07 08:56:22 +0200

    first commit

--- set_git.bat
+++ set_git.bat
@@ -8,5 +8,5 @@ git init
 git add .
 git commit -m "first commit"
 git branch -M main
-git remote add originhttps://github.com/ShorokHeib/Ex8-ReactJS.git
+git remote add origin https://github.com/ShorokHeib/Ex8-ReactJS.git
 git push -u origin main
